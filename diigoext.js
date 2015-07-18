e3dc790d00143de5d52ac10302deb3c3fba034c8
ridermansb <ridermansb@gmail.com>
2015-07-18 11:33:04 -0300

    Add options page

--- diigoext.js
+++ diigoext.js
@@ -1,17 +1,25 @@
-var diigoExtContainerArray = [
-  'div', {'class': 'toolSection diigoext'},
-    ['h2', 'Diigo Ext'],
-    ['div',
-      ['ul',
-        [ 'li',
-          ['span', {'class': 'toolName'},
-            ['a', {}, 'Remove/Merge Duplicate Links']
-          ], ' Remove duplicate links and merge tags'
-        ]
-      ]
-    ]
-  ];
+var startMergeLinks = function() {
+  
+  // Check auth
+  
+  console.log(this);
+};
+
+var el = DOMBuilder.elements;
+var diigoExtElements = el.DIV({'class': 'toolSection diigoext'}, [
+  el.H2('Diigo Ext'),
+  el.DIV([
+    el.UL([
+      el.LI([
+        el.SPAN({'class': 'toolName'}, [
+          el.A({ click: startMergeLinks }, 'Remove/Merge Duplicate Links')
+        ]),
+        ' Remove duplicate links and merge tags'
+      ])
+    ])
+  ])
+]);
 
 var main = document.getElementById('mainInner');
-var diigoExtContainer = DOMBuilder.build(diigoExtContainerArray, 'dom');
-main.insertBefore(diigoExtContainer, main.firstChild);
+
+main.insertBefore(diigoExtElements, main.firstChild);
---
+++ options.js
@@ -0,0 +1,31 @@
+function save_options() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  chrome.storage.local.set({
+    username: username,
+    password: password
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved!';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 1350);
+  });
+}
+
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.local.get({
+    username: '',
+    password: ''
+  }, function(items) {
+    document.getElementById('username').value = items.username;
+    document.getElementById('password').value = items.password;
+    console.log("DiigoExt: Options restored!");
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
